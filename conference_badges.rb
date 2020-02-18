9c413fcf20c282cb3c3f3a9b70dc9e66874a18f7
Terry Threatt <terry.threatt@gmail.com>
2020-02-18 03:05:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,19 +1,13 @@
 # Write your code here.
 
-# problem 1
-def badge_maker(name) 
-  "Hello, my name is #{name}."
-end 
 
-badge_maker("Terry")
-
-
-# problem 2 
 def batch_badge_creator(attendees) 
   
-  attendees.each do |badges|
-    puts "Hello, my name is #{badges}"
+  def badge_maker(name) 
+    "Hello, my name is #{name}."
   end 
+
+  
   
 
 end
